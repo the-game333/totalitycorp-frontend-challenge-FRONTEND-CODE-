88bb7afdecbf23fbe3734dbe79fa4dfd59700320
Abhay <abhaypawar596@gmail.com>
2023-09-22 01:07:21 +0530

    .

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -52,6 +52,50 @@ const ProductList = () => {
     });
   };
 
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `https://ecommerce-myai.onrender.com/api/products?category=${cat}`
+            : "https://ecommerce-myai.onrender.com/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) { }
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
+
+
   return (
     <Container>
       <Navbar />
@@ -88,6 +132,12 @@ const ProductList = () => {
         </Filter>
       </FilterContainer>
       <Products cat={cat} filters={filters} sort={sort} />
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+          .slice(0, 8)
+          .map((item) => <Product item={item} key={item.id} />)}
+
       <Newsletter />
       <Footer />
     </Container>
