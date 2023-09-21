7e15023f2d514bab9305b57b7241b44d122a50cd
Abhay <abhaypawar596@gmail.com>
2023-09-22 01:50:54 +0530

    .

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,11 @@ import styled from "styled-components";
 import { categories } from "../data";
 import { mobile } from "../responsive";
 import CategoryItem from "./CategoryItem";
-
+import {
+  FavoriteBorderOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@material-ui/icons";
 const Container = styled.div`
   display: flex;
   padding: 20px;
@@ -18,7 +22,11 @@ const Categories = () => {
         <CategoryItem item={item} key={item.id} />
       ))}
     </Container>
-      <h2 style={{marginLeft:"350px"}}>Hover below items and Click at 🔍 to proceed..!!</h2>
+      <h2 style={{marginLeft:"350px", fontFamily:"sans-serif"}}>
+        Hover below items and Click at 
+        <Icon>
+          <SearchOutlined />
+        </Icon> to proceed..!!</h2>
       </>
   );
 };
